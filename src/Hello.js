5345fbe2442ebac28f866205356eaf528d46b02b
suchov <artem.suchov@gmail.com>
2019-02-12 22:28:30 +0200

    add Hello js/css and tachyan

---
+++ src/Hello.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+class Hello extends Component {
+  render() {
+    
+    return (      
+      <div className="f1 tc" >
+        <h1>Hello World</h1>
+        <p>Welcome to react</p>
+      </div>
+    )
+  }
+}
+
+export default Hello;
