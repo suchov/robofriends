ca2fe6aaa6ca02431c536b70cc45ec1252b689d2
suchov <artem.suchov@gmail.com>
2019-02-12 22:59:21 +0200

    render multiple same components

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # This is a small robo-friends project to just play/sort robots
+
+BTW we are using [https://robohash.org/](https://robohash.org/) in our app, thanks to them
--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = () => {
   return (
     <div>
-      <img alt='robots' src='' />
+      <img alt='robots' src='https://robohash.org/test?size=200x200' />
       <div>
         <h2>Jane Doe</h2>
         <p>jane.doe@gmail.com</p>
