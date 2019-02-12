1fc235ade70eeea5d9c4aef4f231d41bc6ac9030
suchov <artem.suchov@gmail.com>
2019-02-12 23:46:57 +0200

    add a unik key for React

--- src/CardList.js
+++ src/CardList.js
@@ -3,7 +3,13 @@ import Card from './Card';
 
 const CardList = ({robots}) => {
   const cardsArray = robots.map((user, i) => {
-    return <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    return (
+      <Card 
+        key={robots[i].id} 
+        id={robots[i].id} 
+        name={robots[i].name} 
+        email={robots[i].email} 
+        />)
   })
   return (
     <div>
