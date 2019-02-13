75430310981d5ce58ce95b5005af295295b48152
suchov <artem.suchov@gmail.com>
2019-02-13 07:47:46 +0200

    create a search input

---
+++ src/SearchBox.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBox = () => {
+  return (
+    <input type='search' placeholder='search robots'>
+      
+    </input>
+  )
+}
+
+export default SearchBox;
