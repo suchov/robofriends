c1f67b5f752b60fb437760cfbb353697696a5f5f
suchov <artem.suchov@gmail.com>
2019-02-27 10:14:47 +0200

    fetch the robots form the jsonplaceholder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
 
 class App extends Component {
   constructor(){
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: '',
     }
   }
 
   onSearchChange = (event) => {
     this.setState({ searchField: event.target.value});
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ robots: users }));
   }
 
   render() {
