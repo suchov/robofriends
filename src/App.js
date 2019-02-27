caf9e1d6140d2bcae8f5e014e3ca42426de0a514
suchov <artem.suchov@gmail.com>
2019-02-27 10:19:03 +0200

    add a loading... wording when robots are loading in if/else statement in render

--- src/App.js
+++ src/App.js
@@ -25,13 +25,17 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot => {
       return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     })
-    return (
-      <div className='tc'>
-        <h1>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />  
-      </div>
-    )
+    if (this.state.robots.length === 0){
+      return <h1>Loading...</h1>
+    } else {
+      return (
+        <div className='tc'>
+          <h1>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      )
+    }
   }
 }
 
