ddc388a314a1f90f5ac5e01840aa3613b72f93ac
suchov <artem.suchov@gmail.com>
2019-02-27 11:57:37 +0200

    create folders components and containers to optimize our folder structure and clean it out a bit

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 
 class App extends Component {
   constructor(){
@@ -23,13 +23,13 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const {robots, searchField} = this.state;
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchField.toLowerCase());
     })
-    if (this.state.robots.length === 0){
-      return <h1>Loading...</h1>
-    } else {
-      return (
+    return !robots.length ? 
+      <h1>Loading...</h1> : 
+      (
         <div className='tc'>
           <h1>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
@@ -37,9 +37,8 @@ class App extends Component {
             <CardList robots={filteredRobots} />
           </Scroll>
         </div>
-      )
+      );
     }
-  }
 }
 
 export default App;
